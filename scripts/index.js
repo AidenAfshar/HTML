cbc7fd8e12e4daffa14d1202f8fe2a06c6dae200
Aiden Afshar <aiden.afshar.24@bishops.com>
2022-08-27 17:59:33 -0700

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+alert("This page is very safe, and NOT fraudulent. For real")
+alert("Your adress has been leaked: o̵̳̣̊h̸̡̛̯̊o̶͇͒h̴͖̎̓o̶̯̿i̵̥̦͋̂å̷̢͈h̸͕̬̽s̵̨͆̃d̴͚͂͋")
+alert("You have been seltcted for prize!!!!")
+alert("thank you for signing up for our newsletter")
