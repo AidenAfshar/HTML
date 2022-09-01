8654c399bafd7c3c6238839c2190fe2f515faa9b
Aiden Afshar <aiden.afshar.24@bishops.com>
2022-09-01 10:50:39 -0700

    Revert "Github desktop issues"
    This reverts commit 04cf82a54d2b7f30ff5a1e02caa6d5eb68cec9f7.

---
+++ scripts/ChooseFlashSpeed.js
@@ -0,0 +1,10 @@
+function flashOn() {
+	 document.query
+}
+
+function flashOff() {
+	 window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
+
+document.getElementById("#flashOn").addEventListener("click")
+document.getElementById("#flashOff").addEventListener("click")
